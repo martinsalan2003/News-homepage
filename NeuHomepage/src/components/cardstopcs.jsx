7bad0388bcd2b6a7c043c0bf1e45d0eed26e8d88
AlanMartins00 <alanmartinsferreira59@gmail.com>
2025-01-11 02:29:09 -0300

    teste  componente mobile

--- NeuHomepage/src/components/cardstopcs.jsx
+++ NeuHomepage/src/components/cardstopcs.jsx
@@ -1,9 +1,33 @@
 import '../styles/components-styles/cardstopcs.sass'
 
+import Imgone from '../assets/images/image-retro-pcs.jpg'
+import Imgtwo from '../assets/images/image-top-laptops.jpg'
+import Imgthree from '../assets/images/image-gaming-growth.jpg'
+
+
 export default function Cardtopcs (){
+    const cards = [
+        {id:'01',img:Imgone,title:'Reviving Retro PCs',info:"What happens when old PCs are moderns upgrades"},
+        {id:'02',img:Imgtwo,title:'Top 10 Laptops of 2022',info:'our best picks for various neds and budgets.'},
+        {id:'03',img:Imgthree,title:'The Growth of Gamming',info:'How the pandemic has sparked fresh opportunities'},
+    ]
+    
     return(
-        <section id='topcs'>
-            <h1>cartoes de topcos</h1>
-        </section>
+        <section id="topcs">
+        {cards.map((card) => (
+            <div className="Card" key={card.id}>
+                <div className="container-img">
+                <img src={card.img} alt='imagem' />
+                </div>
+                <div className='card-info'>
+                <h1>{card.id}</h1>
+                <h2>{card.title}</h2>
+                <p>{card.info}</p>
+                </div>
+            </div>
+        ))}
+    </section>
+        
+        
     )
 }
