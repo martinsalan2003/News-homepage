638977c56b4343b38b0368dd28874e1a11120637
AlanMartins00 <alanmartinsferreira59@gmail.com>
2025-01-14 20:32:10 -0300

    inicio do footer

--- NeuHomepage/src/components/footer.jsx
+++ NeuHomepage/src/components/footer.jsx
@@ -2,8 +2,62 @@ import '../styles/components-styles/footer.sass'
 
 export default function Footer(){
     return (
-        <section id="footer">
-            <h1>footer</h1>
-        </section>
-    )
-}
+                   
+        <footer id='footer'>
+            <div className="principal">
+                <div className="img">
+                    <h1>img</h1>
+                </div>
+                <hr />
+                <p>
+                Olá! Sou Alan Martins, desenvolvedor front-end especializado em criar interfaces intuitivas e atraentes. Trabalho com tecnologias como HTML, CSS, JavaScript e frameworks como React e Vue.js.
+                </p>
+                <hr />
+                <div className='icones'>
+                    <img src="https://camo.githubusercontent.com/e6fea164cfe9373591d8b46fd2abd05c3d74f3f400adf9b5946a47fc3eac4e13/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f67682f64657669636f6e732f64657669636f6e406c61746573742f69636f6e732f72656163742f72656163742d6f726967696e616c2e737667" alt="icone-react" />
+                    <img src="https://camo.githubusercontent.com/06c621f8ae3275609119497a65e1a4303662aa5ecb617189064a510494e74e79/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f67682f64657669636f6e732f64657669636f6e406c61746573742f69636f6e732f766974656a732f766974656a732d6f726967696e616c2e737667" alt="icone-vite" />
+                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg" alt="icone-html" />
+                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg" alt="icone-css" />
+                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg" alt="icone-js" />
+                </div>
+            </div>
+            <div class="footer-projetos global-card">
+                <h4>Projetos</h4>
+                <ul>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Customers</a></li>
+                    <li><a href="#">Careers</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                    <li><a href="#">Knowledge Base</a></li>
+                    <li><a href="#">Demo</a></li>
+                </ul>
+            </div>
+            <div class="footer-competencias global-card">
+                <h4>Projetos</h4>
+                <ul>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Customers</a></li>
+                    <li><a href="#">Careers</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                    <li><a href="#">Knowledge Base</a></li>
+                    <li><a href="#">Demo</a></li>
+                </ul>
+            </div>
+            <div class="footer-contatos global-card">
+                <h4>Contatos</h4>
+                <ul>
+                    <li>Tel: (11) 992946537</li>
+                    <li>Email: alanmartinsferreria59@gamil.com</li>
+                    <li><a href="https://github.com/martinsalan2003">Github: AlanMartins00 </a></li>
+                    <li><a href="#">Contact Us</a></li>
+                    <li><a href="#">Knowledge Base</a></li>
+                    <li><a href="#">Demo</a></li>
+                </ul>
+            </div>
+
+            
+        </footer>
+    
+
+        
+)}
